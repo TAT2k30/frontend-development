5fe1ff2e072f69bb003bdd0f99330dd1c90c2ec6
CUONGBUI <cuongbb14@gmail.com>
2024-02-01 15:10:07 +0700

    ngu

--- src/Pages/User/Login/Login.js
+++ src/Pages/User/Login/Login.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { useContext, useState } from 'react';
-import { DataContext } from '../../../Assets/Data/DataContext';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import "./Login.scss"
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { DataContext } from "../../../Assets/Data/DataContext";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import "./Login.scss";
 
 function Login(props) {
   const { login } = useContext(DataContext);
@@ -16,19 +16,24 @@ function Login(props) {
       password: "",
     },
     validationSchema: Yup.object({
-      email: Yup.string().email('Invalid email format').required('Email is required'),
+      email: Yup.string()
+        .email("Invalid email format")
+        .required("Email is required"),
       password: Yup.string()
-        .min(8, 'Your password must be at least 8 characters')
-        .matches(/[a-z]/i, 'Your password must contain at least one letter.')
-        .matches(/[0-9]/, 'Your password must contain at least one digit.')
-        .required('Password is required'),
+        .min(8, "Your password must be at least 8 characters")
+        .matches(/[a-z]/i, "Your password must contain at least one letter.")
+        .matches(/[0-9]/, "Your password must contain at least one digit.")
+        .required("Password is required"),
     }),
     onSubmit: async (values) => {
       setError(null);
       setLoading(true);
 
       try {
-        const response = await axios.post("http://localhost:5085/api/Auth/login", values);
+        const response = await axios.post(
+          "http://localhost:5085/api/Auth/login",
+          values
+        );
         const tokenString = response.data.token;
         console.log("Undecoded token: ", tokenString);
         login(tokenString);
@@ -50,45 +55,74 @@ function Login(props) {
   });
 
   return (
-    <div className='login-form'>
-        <div style={{}}>Check out your revious login</div>
-    <div className="login-container">
-      <form onSubmit={formik.handleSubmit}>
-        {error && <div className="alert" role="alert">{error}</div>}
-        <div className="mb-3 mt-3">
-          <label htmlFor="email" className="form-label"><b>Email:</b></label>
-          <input
-            type="text"
-            className={`form-control ${formik.touched.email && formik.errors.email ? 'is-invalid' : ''}`}
-            id="email"
-            placeholder="Enter email"
-            name="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.touched.email && formik.errors.email && <div className="invalid-feedback">{formik.errors.email}</div>}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label"><b>Password:</b></label>
-          <input
-            type="password"
-            className={`form-control ${formik.touched.password && formik.errors.password ? 'is-invalid' : ''}`}
-            id="password"
-            placeholder="Enter password"
-            name="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
-          {formik.touched.password && formik.errors.password && <div className="invalid-feedback">{formik.errors.password}</div>}
-        </div>
-        <a href="/register">Don't have an account?</a>
-        <button type="submit" className="btn btn-primary" disabled={loading}>
-          {loading ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : "Submit"}
-        </button>
-      </form>
-    </div>
+    <div className="login-form">
+      <div style={{}}>Check out your revious login</div>
+      <div className="login-container">
+        <form onSubmit={formik.handleSubmit}>
+          {error && (
+            <div className="alert" role="alert">
+              {error}
+            </div>
+          )}
+          <div className="mb-3 mt-3">
+            <label htmlFor="email" className="form-label">
+              <b>Email:</b>
+            </label>
+            <input
+              type="text"
+              className={`form-control ${
+                formik.touched.email && formik.errors.email ? "is-invalid" : ""
+              }`}
+              id="email"
+              placeholder="Enter email"
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <div className="invalid-feedback">{formik.errors.email}</div>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              <b>Password:</b>
+            </label>
+            <input
+              type="password"
+              className={`form-control ${
+                formik.touched.password && formik.errors.password
+                  ? "is-invalid"
+                  : ""
+              }`}
+              id="password"
+              placeholder="Enter password"
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <div className="invalid-feedback">{formik.errors.password}</div>
+            )}
+          </div>
+
+          <button type="submit" className="btn btn-primary" disabled={loading}>
+            {loading ? (
+              <span
+                className="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
+            ) : (
+              "Submit"
+            )}
+          </button>
+          <div>
+            <a href="/register">Don't have an account?</a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
