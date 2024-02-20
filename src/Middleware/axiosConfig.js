6a7fc9379d578dee807a6b2f36a983ee37722cb1
BUICUONGG <cuongbb14@gmail.com>
2024-02-20 00:14:41 -0800

    day

--- src/Middleware/axiosConfig.js
+++ src/Middleware/axiosConfig.js
@@ -1,20 +1,22 @@
-import axios from 'axios';
-import { baseUrl } from '../Assets/Data/baseUrl';
+import axios from "axios";
+import { baseUrl } from "../Assets/Data/baseUrl";
 
 const axiosRequest = axios.create({
-    baseURL: baseUrl
+  baseURL: baseUrl,
 });
 
 axiosRequest.interceptors.request.use(function (config) {
-    const token = localStorage.getItem('token');
-    config.headers.Authorization =  token ? `Bearer ${token}` : '';
-    return config;
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
 });
 axiosRequest.interceptors.response.use(
-    response => response.data,
-    error => {
-        console.log("custom",error);
-        return Promise.reject(error);
-    }
+  (response) => response.data,
+  (error) => {
+    console.log("custom", error);
+    return Promise.reject(error);
+  }
 );
 export default axiosRequest;
+
+//làm xấu quát
